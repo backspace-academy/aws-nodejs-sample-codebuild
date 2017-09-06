179434a5c7b6e689a28ccd22be77a80a909d3e8e
BackSpace Academy <31625808+backspace-academy@users.noreply.github.com>
2017-09-06 23:30:12 +1000

    Added test code

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+/*************Test******************/
+var test = require('unit.js');
+var str = 'Hello, world!';
+
+test.string(str).startsWith('Hello');
+
+if (test.string(str).startsWith('Hello')) {
+  console.log('Passed');
+}
+/***********************************/
 var port = process.env.PORT || 3000,
     http = require('http'),
     fs = require('fs'),
