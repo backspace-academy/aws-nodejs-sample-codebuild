f43c24d0ec858bb6adc7195c7714b5cfb29f4372
BackSpace Academy <31625808+backspace-academy@users.noreply.github.com>
2017-09-12 13:46:53 +1000

    .

--- app.js
+++ app.js
@@ -41,3 +41,9 @@ var server = http.createServer(function (req, res) {
         res.end();
     }
 });
+
+// Listen on port 3000, IP defaults to 127.0.0.1
+server.listen(port);
+
+// Put a friendly message on the terminal
+console.log('Server running at http://127.0.0.1:' + port + '/');
